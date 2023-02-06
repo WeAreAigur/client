2f11221600fccbcebc99400e4ed5748f5de7fb3b
Yair Haimo <yair.haimo@gmail.com>
2023-02-07 01:20:02 +0200

    add code hike to docs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const { remarkCodeHike } = require('@code-hike/mdx');
+const theme = require('shiki/themes/material-palenight.json');
 
 const withNextra = require('nextra')({
 	theme: 'nextra-theme-docs',
 	themeConfig: './theme.config.jsx',
+	mdxOptions: {
+		remarkPlugins: [[remarkCodeHike, { theme }]],
+	},
 });
 
 module.exports = withNextra({
--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '#/styles/globals.css';
+import '@code-hike/mdx/dist/index.css';
 
 import type { AppProps } from 'next/app';
 

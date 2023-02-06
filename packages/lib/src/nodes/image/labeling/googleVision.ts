95524f15adb3f534541ea2806483e952a39e0741
Yair Haimo <yair.haimo@gmail.com>
2023-02-06 21:56:06 +0200

    add google vision api

---
+++ packages/lib/src/nodes/image/labeling/googleVision.ts
@@ -0,0 +1,50 @@
+import { z } from 'zod';
+
+const inputSchema = z.object({
+	image: z.string(), // base64
+});
+
+const outputSchema = z.object({
+	labels: z.array(z.string()),
+});
+
+async function action(
+	input: z.input<typeof inputSchema>,
+	apiKeys: Record<string, string>
+): Promise<z.infer<typeof outputSchema>> {
+	const payload = inputSchema.parse(input);
+	const endpoint = `https://vision.googleapis.com/v1/images:annotate?key=${apiKeys.googleapis!}`;
+	const request = {
+		requests: [
+			{
+				image: {
+					content: payload.image,
+				},
+				features: [
+					{
+						type: 'LABEL_DETECTION',
+					},
+				],
+			},
+		],
+	};
+	const response = await fetch(endpoint, {
+		method: 'POST',
+		body: JSON.stringify(request),
+	});
+
+	const data = await response.json();
+
+	return {
+		labels: data.responses[0].labelAnnotations,
+	};
+}
+
+export const googleVisionNode = {
+	id: 'image.labeling.googleVision',
+	schema: {
+		input: inputSchema,
+		output: outputSchema,
+	},
+	action,
+};

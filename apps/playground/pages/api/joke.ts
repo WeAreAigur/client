742fa4b2ab5ff44166b4c41effcdd5f3070cf49a
Yair Haimo <yair.haimo@gmail.com>
2023-02-08 16:48:33 +0200

    remove playground

--- apps/playground/pages/api/joke.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { jokeGptPipeline } from '#/pipelines/jokegpt';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const input = req.body;
-	const output = await jokeGptPipeline.invoke(input);
-	return res.json(output);
-}
--- apps/playground/services/supabase.ts
+++
@@ -1,2 +0,0 @@
-export const supabaseKey = process.env.SUPABASE_SERVICE_KEY!;
-export const supabaseUrl = process.env.SUPABASE_URL!;

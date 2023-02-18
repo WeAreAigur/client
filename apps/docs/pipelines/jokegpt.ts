4e51d6eff75ef1934b1a41bb0ad68b9020d52408
Yair Haimo <yair.haimo@gmail.com>
2023-02-18 20:37:45 +0200

    docs - pipeline validation

--- apps/docs/pipelines/jokegpt.ts
+++ apps/docs/pipelines/jokegpt.ts
@@ -8,19 +8,21 @@ const inputSchema = z.object({
 	subject: z.string(),
 });
 
-export const jokeGptPipeline = aigur.pipeline.create<{ subject: string }, { joke: string }>({
-	id: 'jokegpt',
-	validateInput: validateInput(inputSchema),
-	flow: (flow) =>
-		flow
-			.node(replaceString, ({ input }) => ({
-				text: input.subject,
-				modifier: 'tell me a joke about $(text)$',
-			}))
-			.node(gpt3Prediction, ({ prev }) => ({
-				prompt: prev.text,
-			}))
-			.output(({ prev }) => ({
-				joke: prev.text,
-			})),
-});
+export const jokeGptPipeline = aigur.pipeline.create<z.input<typeof inputSchema>, { joke: string }>(
+	{
+		id: 'jokegpt',
+		validateInput: validateInput(inputSchema),
+		flow: (flow) =>
+			flow
+				.node(replaceString, ({ input }) => ({
+					text: input.subject,
+					modifier: 'tell me a joke about $(text)$',
+				}))
+				.node(gpt3Prediction, ({ prev }) => ({
+					prompt: prev.text,
+				}))
+				.output(({ prev }) => ({
+					joke: prev.text,
+				})),
+	}
+);

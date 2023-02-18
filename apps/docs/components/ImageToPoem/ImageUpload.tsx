f66bb889620eb9f354095f139ac222f0c4f304c6
yairhaimo <yair.haimo@gmail.com>
2023-02-18 21:07:01 +0200

    docs - resize images for poem example (#4)
    * docs - resize images for poem example
    
    * use Compressor.js

--- apps/docs/components/ImageToPoem/ImageUpload.tsx
+++ apps/docs/components/ImageToPoem/ImageUpload.tsx
@@ -1,3 +1,4 @@
+import Compressor from 'compressorjs';
 import { FileUploader } from 'react-drag-drop-files';
 
 export interface ImageType {
@@ -11,15 +12,23 @@ export interface ImageUploadProps {
 
 export function ImageUpload(props: ImageUploadProps) {
 	const onSelect = (file: File) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onloadend = () => {
-			const base64 = (reader.result as string).split('base64,')[1];
-			props.onSelect({
-				base64,
-				name: file.name,
-			});
-		};
+		new Compressor(file, {
+			width: 512,
+			maxWidth: 512,
+			maxHeight: 512,
+			quality: 0.5,
+			success: (result) => {
+				const reader = new FileReader();
+				reader.readAsDataURL(result);
+				reader.onloadend = () => {
+					const base64 = (reader.result as string).split('base64,')[1];
+					props.onSelect({
+						base64,
+						name: file.name,
+					});
+				};
+			},
+		});
 	};
 	return (
 		<FileUploader

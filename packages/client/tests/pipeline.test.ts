5b88300834fe51b960e01b60e300433cbe2282e5
yairhaimo <yair.haimo@gmail.com>
2023-02-22 13:39:57 +0200

    add basic tests (#22)
    * add basic tests
    
    * configure turborepo for tests
    
    * break tests on purpose
    
    * fix tests

---
+++ packages/client/tests/pipeline.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from 'vitest';
+
+import { createClient, replaceString } from '../src/index';
+
+test('simple pipeline', async () => {
+	const aigur = createClient({ apiKeys: {} });
+	const pipeline = aigur.pipeline.create<{ text: string }, { message: string }>({
+		id: 'testPipeline',
+		flow: (flow) =>
+			flow
+				.node(replaceString, ({ input }) => ({
+					text: input.text,
+					modifier: 'Hello $(text)$!',
+				}))
+				.output(({ prev }) => ({
+					message: prev.text,
+				})),
+	});
+
+	const result = await pipeline.invoke({ text: 'world' });
+
+	expect(result).toStrictEqual({ message: 'Hello world!' });
+});
+
+test('multiple nodes', async () => {
+	const aigur = createClient({ apiKeys: {} });
+	const pipeline = aigur.pipeline.create<{ text: string }, { message: string }>({
+		id: 'testPipeline',
+		flow: (flow) =>
+			flow
+				.node(replaceString, ({ input }) => ({
+					text: input.text,
+					modifier: 'Hello $(text)$!',
+				}))
+				.node(replaceString, ({ prev }) => ({
+					text: prev.text,
+					modifier: 'Hello $(text)$!',
+				}))
+				.node(replaceString, ({ prev }) => ({
+					text: prev.text,
+					modifier: 'Hello $(text)$!',
+				}))
+				.node(replaceString, ({ prev }) => ({
+					text: prev.text,
+					modifier: 'Hello $(text)$!',
+				}))
+				.output(({ prev }) => ({
+					message: prev.text,
+				})),
+	});
+
+	const result = await pipeline.invoke({ text: 'world' });
+
+	expect(result).toStrictEqual({ message: 'Hello Hello Hello Hello world!!!!' });
+});

d81b56a98f603f0b445bf0c3c9d553499b38e940
Yair Haimo <yair.haimo@gmail.com>
2023-02-27 00:15:33 +0200

    docs - logs

--- apps/docs/pages/api/pipelines/[id].ts
+++ apps/docs/pages/api/pipelines/[id].ts
@@ -6,14 +6,18 @@ import { vercelEdgeFunction } from '@aigur/client';
 
 export default async function handler(req: NextRequest) {
 	const { userId, input } = await req.clone().json();
+	console.log(`***spammerIds`, spammerIds);
 	console.log(`***userId`, userId, input);
 	if (spammerIds.includes(userId)) {
+		console.log(`spammer!`);
 		return new Response(JSON.stringify({}, null, 2), {
 			status: 500,
 			headers: {
 				'content-type': 'application/json;charset=UTF-8',
 			},
 		});
+	} else {
+		console.log(`not spammer!`);
 	}
 	return vercelEdgeFunction(pipelines)(req);
 }

ecbc9e211eadb45d3d3246634436a2967e42f00e
Yair Haimo <yair.haimo@gmail.com>
2023-02-27 14:27:36 +0200

    docs - chat be considerate message

--- apps/docs/services/spammers.ts
+++ apps/docs/services/spammers.ts
@@ -1,2 +1,2 @@
 // TODO: migrate to redis
-export const spammerIds = ['vyt968pycrbfywqo', 'x4lwznbeoa7bxtmz'];
+export const spammerIds = ['vyt968pycrbfywqo', 'x4lwznbeoa7bxtmz', '0jclnx3m2roqqalz'];

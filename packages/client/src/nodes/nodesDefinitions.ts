c52b87a383aff27db5fb2c4c15f3154f3b6366f4
yairhaimo <yair.haimo@gmail.com>
2023-03-02 11:37:02 +0200

    Gpt3 node defaults to turbo (#38)
    * gpt3 defaults to turbo
    
    * changeset

--- packages/client/src/nodes/nodesDefinitions.ts
+++ packages/client/src/nodes/nodesDefinitions.ts
@@ -9,7 +9,7 @@ export * from './text/modify/replaceMultipleStrings';
 export * from './text/modify/replaceString';
 export { gpt3Prediction } from './text/prediction/gpt3';
 export { gpt3TurboPrediction } from './text/prediction/gpt3-turbo';
-export { gpt3TurboPredictionStream as gpt3TurboPredictionStream } from './text/prediction/gpt3-turbo.stream';
+export { gpt3TurboPredictionStream } from './text/prediction/gpt3-turbo.stream';
 export { gpt3PredictionStream } from './text/prediction/gpt3.stream';
 export * from './transformation/stringToArrayBuffer';
 export * from './voice/textToSpeech/googleTextToSpeech';
